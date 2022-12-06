3ae0c670e24aa8f41c7d91209cbf19ee4a1a25da
katzkean <al.katzke@gmail.com>
2022-12-06 16:09:55 +0100

    Work on configuration

--- refactoring/import_config.py
+++ refactoring/import_config.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import yaml
 from typing import Literal
+from dacite import
 
 from refactoring.rules import STRENGTH_TYPE, assess_pp3
 
@@ -12,12 +13,20 @@ def check_dictionary_for_function(rules)
 
 @dataclass
 class Configuration():
-    rules = dict[str, dict[str, str]]
-    prediction_tools_threshold = dict[str, int]
-    allele_frequency_threshold = dict[str, int]
+    rules: dict[str, dict]
+    prediction_tools_threshold: dict[str, dict]
+    allele_frequency_threshold: dict[str, int]
 
 
     def load_config(path_yaml: str) -> Configuration:
         with open(path_yaml) as config_file:
             config = yaml.safe_load(config_file)
          return config
+
+@dataclass
+class Prediction_too_threshold():
+    predictions: dict[str, int]
+
+@dataclass
+class Allele_frequency_thresholds():
+    thresholds: dict[str, int]
