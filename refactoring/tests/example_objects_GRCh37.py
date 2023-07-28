ab9e07bf5e55583eb8acae28114c0c2a246943f9
akatzke <al.katzke@gmail.com>
2023-07-28 16:20:54 +0200

    New examples for testing NMD, construct sequence and exon skipping

---
+++ refactoring/tests/example_objects_GRCh37.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+
+from variant import VariantInfo, TranscriptInfo
+
+import hgvs.parser
+import hgvs.posedit
+
+hgvs_parser = hgvs.parser.Parser()
+
+
+def create_example_splice_acceptor() -> tuple(TranscriptInfo, VariantInfo):
+    """
+    Create BRCA1 splice acceptor variant
+    From HerediVar
+    """
+    hgvs = hgvs_parser.parse_c_posedit("c.5278-1G>A".split("c.")[1])
+    transcript = TranscriptInfo(
+        "ENST00000357654", "splice_acceptor", hgvs, 5278, 5278, intron=19
+    )
+    variant = VariantInfo(
+        "BRCA1",
+        ["splice_acceptor"],
+        "17",
+        41203135,
+        41203135,
+        "some_id",
+        "C",
+        "T",
+    )
+    return (transcript, variant)
+
+
+def create_example_splice_donor() -> tuple(TranscriptInfo, VariantInfo):
+    """
+    Create BRCA1 splice donor variant
+    From ClinVar: VCV125738.12
+    """
+    hgvs = hgvs_parser.parse_c_posedit("c.4986+1G>T".split("c.")[1])
+    transcript = TranscriptInfo("ENST00000357654", "splice_donor", hgvs, 4986, 4986)
+    variant = VariantInfo(
+        "BRCA1",
+        ["splice_donor"],
+        "17",
+        41222944,
+        41222944,
+        "some_id",
+        "C",
+        "T",
+        intron=15,
+    )
+    return (transcript, variant)
+
+
+def create_example_dup() -> tuple:
+    """
+    Create PMS2 duplication variant
+    From HerediVar
+    """
+    hgvs = hgvs_parser.parse_c_posedit("c.1831dup".split("c.")[1])
+    transcript = TranscriptInfo(
+        "ENST00000265849", "frameshift_variant", hgvs, 1831, 1832, exon=11
+    )
+    variant = VariantInfo(
+        "PMS2",
+        ["frameshift_variant"],
+        "7",
+        6026564,
+        6026565,
+        "some_id",
+        "A",
+        "AT",
+    )
+    return (transcript, variant)
+
+
+def create_example_ins() -> tuple:
+    """
+    Create BRCA1 insertion variant
+    From ClinVar: VCV000266352.6
+    """
+    hgvs = hgvs_parser.parse_c_posedit("c.330_331insA".split("c.")[1])
+    transcript = TranscriptInfo(
+        "ENST00000357654", "frameshift_variant", hgvs, 330, 331, exon=6
+    )
+    variant = VariantInfo(
+        "BRCA1", ["frameshift_variant"], "17", 41256249, 41256250, "some_id", "", "T"
+    )
+    return (transcript, variant)
+
+
+def create_example_del() -> tuple:
+    """
+    Create TP53 deletion variant
+    From HerediVar
+    """
+    hgvs = hgvs_parser.parse_c_posedit("c.286_288del".split("c.")[1])
+    transcript = TranscriptInfo(
+        "ENST0000269305", "inframe_deletion", hgvs, 286, 288, exon=4
+    )
+    variant = VariantInfo(
+        "TP53", ["inframe_deletion"], "17", 7579399, 7579401, "some_id", "CAGA", "C"
+    )
+    return (transcript, variant)
+
+
+def create_example_indel() -> tuple:
+    """
+    Create BRCA1 indel variant
+    From HerediVar
+    """
+    hgvs = hgvs_parser.parse_c_posedit("c.4391_4393delinsTT".split("c.")[1])
+    transcript = TranscriptInfo(
+        "ENST00000357654", "frameshift_variant", hgvs, 4391, 4393, exon=13
+    )
+    variant = VariantInfo(
+        "BRCA1",
+        ["frameshift_variant"],
+        "17",
+        41228596,
+        41228598,
+        "some_id",
+        "TAG",
+        "AA",
+    )
+    return (transcript, variant)
+
+
+def create_example_snv_mis() -> tuple:
+    """
+    Create BRCA1 missense SNV variant
+    From HerediVar
+    """
+    hgvs = hgvs_parser.parse_c_posedit("c.5219T>G".split("c.")[1])
+    transcript = TranscriptInfo(
+        "ENST00000357654", "missense_variant", hgvs, 5219, 5219, exon=19
+    )
+    variant = VariantInfo(
+        "BRCA1", ["missense_variant"], "17", 41209127, 41209127, "some_id", "A", "C"
+    )
+    return (transcript, variant)
+
+
+def create_example_snv_syn() -> tuple:
+    """
+    Create BRCA1 synonymous SNV variant
+    From HerediVar
+    """
+    hgvs = hgvs_parser.parse_c_posedit("c.1740C>T".split("c.")[1])
+    transcript = TranscriptInfo(
+        "ENST00000357654", "synonymous_variant", hgvs, 4986, 4986, exon=10
+    )
+    variant = VariantInfo(
+        "BRCA1", ["synonymous_variant"], "17", 41245808, 4125808, "some_id", "G", "A"
+    )
+    return (transcript, variant)
