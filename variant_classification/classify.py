db8494d96f7649d1c595ab089a38f19c262d1683
Athena <al.katzke@gmail.com>
2023-12-12 14:17:48 +0100

    Added disease relevant transcript

--- variant_classification/classify.py
+++ variant_classification/classify.py
@@ -6,6 +6,7 @@ import argparse
 from ensembl import ensembl
 from load_config import load_config, get_gene_specific_config
 from load_variant import load_variant
+from check_disease_relevant_transcript import check_disease_relevant_transcript
 from information import Classification_Info
 from config_annotation import (
     get_annotations_needed_from_rules,
@@ -31,13 +32,14 @@ def classify(config_path: pathlib.Path, variant_str: str):
     configuration_name, final_config = get_gene_specific_config(
         config, variant.variant_info.gene_name
     )
+    variant_disease_relevant = check_disease_relevant_transcript(variant, final_config)
     class_info = Classification_Info()
     annotations_needed_by_rules = get_annotations_needed_from_rules(
         final_config["rules"], class_info
     )
     annotations_needed = get_unique_annotations_needed(annotations_needed_by_rules)
     annotations_set_up = get_annotation_functions(
-        annotations_needed, variant, final_config, class_info
+        annotations_needed, variant_disease_relevant, final_config, class_info
     )
     annotation = execute_annotation(annotations_set_up)
     annotations_needed_by_rules_filtered = remove_rules_with_missing_annotation(
