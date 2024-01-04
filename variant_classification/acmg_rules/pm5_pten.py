9afab1a182a979dcaf2c4073e8a7abd3d3fb62c9
akatzke <al.katzke@gmail.com>
2024-01-04 16:01:17 +0100

    Implementation gene specific PM5 rules

---
+++ variant_classification/acmg_rules/pm5_pten.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+from typing import Callable
+
+from acmg_rules.utils import (
+    RuleResult,
+    evidence_strength,
+    abstract_rule,
+    evidence_type,
+    rule_type,
+)
+from information import Info, Classification_Info
+from clinvar_utils import ClinVar_Type, ClinVar_Status, ClinVar_similarity
+from variant_classification.variant import AffectedRegion
+
+
+class Pm5_protein_pten(abstract_rule):
+    """
+    PM5: Pathogenic missense variant to different amino acid in same position classified as pathogenic in ClinVar
+    Including the gene specifications for PTEN
+    """
+
+    @classmethod
+    def get_assess_rule(
+        cls, class_info: Classification_Info
+    ) -> tuple[Callable, tuple[Info, ...]]:
+        return (
+            cls.assess_rule,
+            (
+                class_info.VARIANT_CLINVAR_SPLICEAI_PROT_BLOSUM62,
+                class_info.VARIANT_HOTSPOT,
+            ),
+        )
+
+    @classmethod
+    def assess_rule(
+        cls,
+        clinvar_results: dict[ClinVar_Type, ClinVar_similarity],
+        variant_in_hotspot: AffectedRegion,
+    ) -> RuleResult:
+        clinvar_diff_aa = clinvar_results[ClinVar_Type.DIFF_AA_CHANGE]
+        if variant_in_hotspot:
+            return RuleResult(
+                "PM5",
+                rule_type.PROTEIN,
+                evidence_type.PATHOGENIC,
+                False,
+                evidence_strength.MODERATE,
+                comment=f"Variant located in mutational hotspot. PM5 is not applicable.",
+            )
+        if (
+            clinvar_diff_aa.pathogenic
+            and clinvar_diff_aa.highest_classification == ClinVar_Status.PATHOGENIC
+        ):
+            comment = f"The following ClinVar entries show an amino acid change in the same position as pathogenic: {clinvar_diff_aa.ids}."
+            result = True
+        else:
+            comment = "No ClinVar entries found that show an amino acid change in the same position as pathogenic."
+            result = False
+        return RuleResult(
+            "PM5",
+            rule_type.PROTEIN,
+            evidence_type.PATHOGENIC,
+            result,
+            evidence_strength.MODERATE,
+            comment,
+        )
