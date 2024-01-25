ae6c261a1db12e1b197ebdd2322f07789368b8c8
akatzke <al.katzke@gmail.com>
2024-01-25 15:31:43 +0100

    Fixed issue with bed intersection

--- variant_classification/check_coldspot_hotspot.py
+++ variant_classification/check_coldspot_hotspot.py
@@ -4,10 +4,14 @@ import pathlib
 
 from collections.abc import Callable
 
+import pyensembl
+
+from pybedtools import BedTool
+
 from ensembl import ensembl
 from information import Classification_Info, Info
 from variant import TranscriptInfo, VariantInfo
-from utils import check_intersection_with_bed
+from utils import create_bed_line
 
 
 def check_variant_intersection_with_bed(
@@ -22,7 +26,7 @@ def check_variant_intersection_with_bed(
     ref_transcript = ensembl.transcript_by_id(transcript[0].transcript_id)
     var_start = variant.genomic_start
     var_end = variant.genomic_end
-    is_in_hotspot = check_intersection_with_bed(
+    is_in_hotspot = check_intersection_with_bed_no_strand(
         variant, var_start, var_end, ref_transcript, variant_hotspot_annotation_path
     )
     return is_in_hotspot
@@ -58,3 +62,24 @@ def get_check_coldspot(
             class_info.TRANSCRIPT,
         ),
     )
+
+
+def check_intersection_with_bed_no_strand(
+    variant: VariantInfo,
+    gen_start: int,
+    gen_end: int,
+    ref_transcript: pyensembl.transcript.Transcript,
+    path_bed: pathlib.Path,
+) -> bool:
+    """
+    Check if variant overlaps UniProt annotated repetitive region
+    """
+    variant_interval = BedTool(
+        create_bed_line(variant, gen_start, gen_end, ref_transcript.strand),
+        from_string=True,
+    )[0]
+    bed = BedTool(path_bed).sort()
+    annotation_hits = bed.all_hits(variant_interval)
+    if len(annotation_hits) > 0:
+        return True
+    return False
