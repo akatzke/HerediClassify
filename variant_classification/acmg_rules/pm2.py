d1d8ecdb0af5c2800270ef624a55d61832ee0cf3
akatzke <al.katzke@gmail.com>
2024-01-30 11:23:33 +0100

    Added Enigma specific PM2 rule

--- variant_classification/acmg_rules/pm2.py
+++ variant_classification/acmg_rules/pm2.py
@@ -10,7 +10,7 @@ from acmg_rules.utils import (
     evidence_type,
 )
 from information import Classification_Info, Info
-from variant import PopulationDatabases_gnomAD
+from variant import PopulationDatabases_gnomAD, VariantInfo
 
 
 class Pm2(abstract_rule):
@@ -63,7 +63,7 @@ class Pm2_supporting(abstract_rule):
     """
     PM2: Varinat is absent from control population
     In case of recessive disorders: Variant occurres less than expected carrier rate
-    Default strength of PM2 is set to supporting
+    Default strength of PM2 is set to supporting following SVI recommendations
     """
 
     @classmethod
@@ -83,6 +83,61 @@ class Pm2_supporting(abstract_rule):
         cls, gnomad: PopulationDatabases_gnomAD, threshold_pm2: float
     ) -> RuleResult:
         if gnomad.popmax_frequency is None:
+            raise ValueError(
+                f"The gnomAD allele frequency is None. Please check variant import."
+            )
+        elif gnomad.popmax_frequency <= threshold_pm2:
+            comment = f"Variant occures with {gnomad.popmax_frequency} in gnomAD subpopulation {gnomad.popmax}."
+            result = True
+        else:
+            comment = f"Variant occures with {gnomad.popmax_frequency} in gnomAD subpopulation {gnomad.popmax}."
+            result = False
+        if gnomad.popmax == "None":
+            comment = f"Variant does not occur in gnomAD, allele frequency in gnomAD is assumed to be 0."
+        if gnomad.popmax == "ALL":
+            comment = f"Variant has no popmax. Variant occurs with {gnomad.popmax_frequency} in gnomAD."
+        return RuleResult(
+            "PM2",
+            rule_type.GENERAL,
+            evidence_type.PATHOGENIC,
+            result,
+            evidence_strength.SUPPORTING,
+            comment,
+        )
+
+
+class Pm2_supporting_no_ins_del_indel(abstract_rule):
+    """
+    PM2: Varinat is absent from control population
+    In case of recessive disorders: Variant occurres less than expected carrier rate
+    Default strength of PM2 is set to supporting following SVI recommendations
+    PM2 does not apply to insertions, deletions or indels
+    """
+
+    @classmethod
+    def get_assess_rule(
+        cls, class_info: Classification_Info
+    ) -> tuple[Callable, tuple[Info, ...]]:
+        return (
+            cls.assess_rule,
+            (
+                class_info.VARIANT,
+                class_info.VARIANT_GNOMAD,
+                class_info.THRESHOLD_PM2,
+            ),
+        )
+
+    @classmethod
+    def assess_rule(
+        cls,
+        variant: VariantInfo,
+        gnomad: PopulationDatabases_gnomAD,
+        threshold_pm2: float,
+    ) -> RuleResult:
+        if len(variant.var_ref) != 1 or len(variant.var_obs) != 1:
+            comment = f"PM2 does not apply to insertions, deletions or delins."
+            result = False
+        elif gnomad.popmax_frequency is None:
             raise ValueError(
                 f"The gnomAD allele frequency is None. Please check variant import."
             )
