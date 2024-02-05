6339c2484e54bcfa2b15717bdbf2e21f95f9687c
akatzke <al.katzke@gmail.com>
2024-02-05 16:19:35 +0100

    Finished first version of script for critical regions

--- install_dependencies/data_create_uniprot_critical_domains.py
+++ install_dependencies/data_create_uniprot_critical_domains.py
@@ -7,8 +7,10 @@ import pandas as pd
 import numpy as np
 
 from cyvcf2 import VCF
+import pyensembl
 
 from variant_classification.clinvar_utils import convert_vcf_gen_to_df
+
 
 parser = argparse = argparse.ArgumentParser()
 
@@ -30,6 +32,8 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
+ensembl = pyensembl.EnsemblRelease(110)
+
 
 def create_uniprot_critical_domains(
     uniprot_path: pathlib.Path, clinvar_path: pathlib.Path, out_path: pathlib.Path
@@ -37,30 +41,47 @@ def create_uniprot_critical_domains(
     """
     From UniProt and ClinVar create disease relevant transcript
     """
-    uniprot = pd.read_csv(uniprot_path, sep="\t")
     clinvar = VCF(clinvar_path)
+    uniprot = pd.read_csv(uniprot_path, sep="\t")
     uniprot.rename(columns={"#chrom": "chrom"}, inplace=True)
-    uniprot_out = uniprot.assign(
+    uniprot_chrom = uniprot[uniprot.chrom.str.match("chr[0-9]+$")]
+    uniprot_out = uniprot_chrom.assign(
         count_lp_p=np.nan,
         count_lb_b=np.nan,
         count_vus=np.nan,
-        pathogenic_probability=np.nan,
+        pathogenicity_score=np.nan,
     )
     uniprot_patho_score = uniprot_out.apply(
-        calculate_pathogenicity_score, clinvar=clinvar, axis=1
+        annotate_pathogenicity_score, clinvar=clinvar, axis=1
     )
+    critical_domains = uniprot_patho_score[
+        (uniprot_patho_score.pathogenicity_score >= 0.51)
+        & (uniprot_patho_score.count_lp_p >= 5)
+        & (uniprot_patho_score.count_lb_b == 0)
+    ]
+    return critical_domains
 
 
-def calculate_pathogenicity_score(
+def annotate_pathogenicity_score(
     domain: pd.Series, clinvar: cyvcf2.cyvcf2.VCF
 ) -> pd.Series:
     """
     Calculate pathogenicity score for a domain
     """
     overlapping_clinvar = get_clinvar_entries(clinvar, domain)
-    count_by_classification = get_count_by_classification(overlapping_clinvar)
-    out = create_output_entry(domain, count_by_classification)
-    return out
+    if overlapping_clinvar.empty:
+        domain.pathogenicity_score = 0
+        domain.count_lp_p = 0
+        domain.count_lb_b = 0
+        domain.count_vus = 0
+        return domain
+    count_by_class = get_count_by_classification(overlapping_clinvar)
+    pathogenicity_score = calculate_pathogenicity_score(count_by_class)
+    domain.pathogenicity_score = pathogenicity_score
+    domain.count_lp_p = count_by_class["num_p_lp"]
+    domain.count_lb_b = count_by_class["num_b_lb"]
+    domain.count_vus = count_by_class["num_VUS"]
+    return domain
 
 
 def get_clinvar_entries(clinvar: cyvcf2.cyvcf2.VCF, domain: pd.Series) -> pd.DataFrame:
@@ -71,15 +92,64 @@ def get_clinvar_entries(clinvar: cyvcf2.cyvcf2.VCF, domain: pd.Series) -> pd.Dat
         f"{domain.chrom.split('chr')[1]}:{domain.chromStart}-{domain.chromEnd}"
     )
     clinvar_region_df = convert_vcf_gen_to_df(clinvar_region)
-    return clinvar_region_df
-
-
-def get_count_by_classification(clinvars: pd.DataFrame) -> dict[str:int]:
+    if clinvar_region_df.empty:
+        return clinvar_region_df
     quality_two_star = [
         "reviewed_by_expert_panel",
         "criteria_provided,_multiple_submitters,_no_conflicts",
     ]
-    clinvars_quality = clinvars[clinvars.CLNREVSTAT.isin(quality_two_star)]
+    clinvar_quality = clinvar_region_df[
+        clinvar_region_df.CLNREVSTAT.isin(quality_two_star)
+    ]
+    if clinvar_quality.empty:
+        return clinvar_quality
+    clinvar_filtered = filter_by_strand(clinvar_quality, domain.strand)
+    return clinvar_filtered
+
+
+def filter_by_strand(clinvar_entries: pd.DataFrame, strand: str) -> pd.DataFrame:
+    """
+    Check for all clinvar entries if gene is on the same strand as the protein domain
+    """
+    # Format GENEINFO column to only contain gene name
+    clinvar_entries["gene"] = clinvar_entries.GENEINFO.str.split(":", expand=True)[0]
+    unique_genes = list(clinvar_entries.gene.unique())
+    genes_to_remove = []
+    for gene in unique_genes:
+        try:
+            ensembl_genes = ensembl.genes_by_name(gene)
+        except ValueError:
+            continue
+        for ensembl_gene in ensembl_genes:
+            if ensembl_gene.strand != strand:
+                genes_to_remove.append(gene)
+    filtered_clinvar = clinvar_entries[~clinvar_entries.gene.isin(genes_to_remove)]
+    return filtered_clinvar
+
+
+def get_count_by_classification(clinvar: pd.DataFrame) -> dict[str, int]:
+    """
+    Filter ClinVar for entreis
+    """
+    num_VUS = clinvar[clinvar.CLNSIG == "Uncertain_significance"].shape[0]
+    num_b_lb = clinvar[clinvar.CLNSIG.str.contains("benign", case=False)].shape[0]
+    num_p_lp = clinvar[clinvar.CLNSIG.str.contains("pathogenic", case=False)].shape[0]
+    return {"num_VUS": num_VUS, "num_b_lb": num_b_lb, "num_p_lp": num_p_lp}
+
+
+def calculate_pathogenicity_score(count_by_class: dict[str, int]) -> float:
+    """
+    Calcualte pathogenicity score
+    """
+    delta = 10 ** (-6)
+    total = sum(
+        [
+            count_by_class["num_b_lb"] + delta,
+            count_by_class["num_p_lp"] + delta,
+            count_by_class["num_VUS"] + delta,
+        ]
+    )
+    return (count_by_class["num_p_lp"] + delta) / total
 
 
 def main():
