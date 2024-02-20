0c40bada5e3300d1e1ea8a2cc9c719d6a93a5ae9
akatzke <al.katzke@gmail.com>
2024-02-20 12:41:30 +0100

    Debugging functional and splicing assay utils

--- variant_classification/acmg_rules/functional_splicing_assay_utils.py
+++ variant_classification/acmg_rules/functional_splicing_assay_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import logging
+from typing import Optional
 
 from variant import ALLELIC, FunctionalData, RNAData
 from acmg_rules.utils import RuleResult, evidence_strength
@@ -36,88 +37,129 @@ def adjust_strength_according_to_rna_data_pvs1(
     Modify strength of PVS1 according to available RNA data
     """
     no_quantification = 0
-    minigene_90, minigene, patient_90, patient, patient_no_allele_specific = (
-        0,
-        0,
-        0,
-        0,
-        0,
-    )
+    minigene_90 = False
+    minigene = False
+    patient_90 = False
+    patient = False
+    patient_no_allele_specific_patho = False
+    patient_no_allele_specific_ben = False
+    inconclusive = False
+    benign = False
     for entry in rna_data:
         if not entry.quantification or entry.quantification is None:
             no_quantification += 1
-        if entry.minigene and entry.allelic is ALLELIC.CONSTRUCT:
+        elif entry.minigene and entry.allelic.value is ALLELIC.CONSTRUCT.value:
             if entry.quantification >= 0.9:
-                minigene_90 += 1
+                minigene_90 = True
             elif entry.quantification < 0.9 and entry.quantification > 0.8:
-                minigene += 0
+                minigene = True
+            elif entry.quantification < 0.7:
+                benign = True
+            else:
+                inconclusive = True
         elif entry.patient_rna:
-            if entry.allelic is ALLELIC.TRUE:
+            if entry.allelic.value is ALLELIC.TRUE.value:
                 if entry.quantification >= 0.9:
-                    patient_90 += 1
+                    patient_90 = True
                 elif entry.quantification < 0.9 and entry.quantification > 0.8:
-                    patient += 1
-            elif entry.allelic is ALLELIC.FALSE and entry.quantification >= 0.5:
-                patient_no_allele_specific += 1
+                    patient = True
+                elif entry.quantification < 0.7:
+                    benign = True
+                else:
+                    inconclusive = True
+            elif entry.allelic.value is ALLELIC.FALSE.value:
+                if entry.quantification >= 0.45:
+                    patient_no_allele_specific_patho = True
+                elif entry.quantification <= 0.05:
+                    patient_no_allele_specific_ben = True
+                else:
+                    inconclusive = True
     strength_adjustment_by_one = {
-        evidence_strength.VERY_STRONG: evidence_strength.STRONG,
-        evidence_strength.STRONG: evidence_strength.MODERATE,
-        evidence_strength.MODERATE: evidence_strength.SUPPORTING,
-        evidence_strength.SUPPORTING: evidence_strength.SUPPORTING,
+        evidence_strength.VERY_STRONG.value: evidence_strength.STRONG,
+        evidence_strength.STRONG.value: evidence_strength.MODERATE,
+        evidence_strength.MODERATE.value: evidence_strength.SUPPORTING,
+        evidence_strength.SUPPORTING.value: evidence_strength.SUPPORTING,
     }
     strength_adjustment_by_two = {
-        evidence_strength.VERY_STRONG: evidence_strength.MODERATE,
-        evidence_strength.STRONG: evidence_strength.SUPPORTING,
-        evidence_strength.MODERATE: evidence_strength.SUPPORTING,
-        evidence_strength.SUPPORTING: evidence_strength.SUPPORTING,
+        evidence_strength.VERY_STRONG.value: evidence_strength.MODERATE,
+        evidence_strength.STRONG.value: evidence_strength.SUPPORTING,
+        evidence_strength.MODERATE.value: evidence_strength.SUPPORTING,
+        evidence_strength.SUPPORTING.value: evidence_strength.SUPPORTING,
     }
     strength_adjustment_no_allelic_quant = {
-        evidence_strength.VERY_STRONG: evidence_strength.STRONG,
-        evidence_strength.STRONG: evidence_strength.MODERATE,
-        evidence_strength.MODERATE: evidence_strength.MODERATE,
-        evidence_strength.SUPPORTING: evidence_strength.MODERATE,
+        evidence_strength.VERY_STRONG.value: evidence_strength.STRONG,
+        evidence_strength.STRONG.value: evidence_strength.MODERATE,
+        evidence_strength.MODERATE.value: evidence_strength.MODERATE,
+        evidence_strength.SUPPORTING.value: evidence_strength.MODERATE,
     }
     if not rna_data:
-        result.comment = result.comment + " No RNA assay performed."
-    elif no_quantification == len(rna_data):
-        result.comment = (
-            result.comment
-            + " No quantification available for the RNA assay. Please check resulst of RNA assay manually."
-        )
-    if patient_90 > 0 and patient == 0:
-        result.name = "PVS1_RNA"
-        result.comment = (
-            result.comment
-            + " A splicing assay was performed with allele_specific quantification using patient RNA showing >=90% proportion of non-functional transcript."
-        )
-    elif patient > 0:
-        result.name = "PVS1_RNA"
-        result.strength = strength_adjustment_by_one[result.strength]
-        result.comment = (
-            result.comment
-            + " A splicing assay was performed with allele_specific quantification using patient RNA showing <90% and >80% proportion of non-functional transcript."
-        )
-    elif minigene_90 > 0 and minigene == 0:
-        result.name = "PVS1_RNA"
-        result.strength = strength_adjustment_by_one[result.strength]
-        result.comment = (
-            result.comment
-            + " A mingene assay was performed showing >=90% proportion of non-functional transcript."
-        )
-    elif minigene:
-        result.name = "PVS1_RNA"
-        result.strength = strength_adjustment_by_two[result.strength]
-        result.comment = (
-            result.comment
-            + " A minigene assay was performed showing <90% and >80% proportion of non-functional transcript."
-        )
-    elif patient_no_allele_specific > 0:
-        result.strength = strength_adjustment_no_allelic_quant[result.strength]
-        result.comment = (
-            result.comment
-            + " A splicing assay was performed with quantification (no allele-speicific quantification) using patient RNA showing >=50% proportion of non-functional transcript from both alleles."
-        )
+        new_comment = "No RNA assay performed."
+        update_rule_result(result, comment=new_comment)
+        return result
+    if no_quantification == len(rna_data):
+        new_comment = "No quantification available for the RNA assay. Please check results of RNA assay manually."
+        update_rule_result(result, comment=new_comment)
+        return result
+    result.name = "PVS1_RNA"
+    if inconclusive:
+        new_comment = "The RNA assay results are inconclusive. Please check results of RNA assay manually."
+        update_rule_result(result, status=False, comment=new_comment)
+        return result
+    if (benign or patient_no_allele_specific_ben) and (
+        minigene
+        or patient
+        or minigene_90
+        or patient_90
+        or patient_no_allele_specific_patho
+    ):
+        new_comment = "The RNA assays show contradicting evidence. PVS1_RNA does not apply. Please check resulst of RNA assay manually."
+        update_rule_result(result, status=False, comment=new_comment)
+        return result
+    if benign or patient_no_allele_specific_ben:
+        new_comment = "A splicing assay shows no effect on splicing for this variant. PVS1_RNA does therfore not apply."
+        update_rule_result(result, status=False, comment=new_comment)
+        return result
+    if patient_90 and not patient:
+        new_comment = "A splicing assay was performed with allele_specific quantification using patient RNA showing >=90% proportion of non-functional transcript."
+        update_rule_result(result, comment=new_comment)
+        return result
+    if patient:
+        strength = strength_adjustment_by_one[result.strength.value]
+        new_comment = "A splicing assay was performed with allele_specific quantification using patient RNA showing <90% and >80% proportion of non-functional transcript."
+        update_rule_result(result, strength=strength, comment=new_comment)
+        return result
+    if minigene_90 and not minigene:
+        strength = strength_adjustment_by_one[result.strength.value]
+        new_comment = "A mingene assay was performed showing >=90% proportion of non-functional transcript."
+        update_rule_result(result, strength=strength, comment=new_comment)
+        return result
+    if minigene:
+        strength = strength_adjustment_by_two[result.strength.value]
+        new_comment = "A minigene assay was performed showing <90% and >80% proportion of non-functional transcript."
+        update_rule_result(result, strength=strength, comment=new_comment)
+        return result
+    if patient_no_allele_specific_patho:
+        strength = strength_adjustment_no_allelic_quant[result.strength.value]
+        new_comment = "A splicing assay was performed with quantification (no allele-speicific quantification) using patient RNA showing >=50% proportion of non-functional transcript from both alleles."
+        update_rule_result(result, strength=strength, comment=new_comment)
+        return result
     return result
+
+
+def update_rule_result(
+    rule_result: RuleResult,
+    comment: str,
+    status: Optional[bool] = None,
+    strength: Optional[evidence_strength] = None,
+):
+    """
+    Update rule result based on classification
+    """
+    if status is not None:
+        rule_result.status = status
+    if strength is not None:
+        rule_result.strength = strength
+    rule_result.comment = rule_result.comment + " " + comment
 
 
 def assess_splicing_data_bp7(
@@ -127,21 +169,23 @@ def assess_splicing_data_bp7(
     Assess splicing data for applicability to BP7
     """
     performed = True
-    no_quantification = 0, 0
-    bp7_allele, bp7_allele_not, bp7, bp7_no = 0, 0, 0, 0
+    no_quantification = 0
+    bp7_allele, bp7_allele_not, bp7, bp7_no = False, False, False, False
     for entry in rna_data:
-        if not entry.quantification or entry.quantification is None:
+        if entry.quantification is None:
             no_quantification = +1
-        elif (entry.patient_rna and entry.allelic is ALLELIC.TRUE) or entry.minigene:
+        elif (
+            entry.patient_rna and entry.allelic.value is ALLELIC.TRUE.value
+        ) or entry.minigene:
             if entry.quantification < 0.7:
-                bp7_allele += 1
+                bp7_allele = True
             else:
-                bp7_allele_not += 1
-        elif entry.patient_rna and entry.allelic is ALLELIC.FALSE:
-            if entry.quantification == 0:
-                bp7 += 1
+                bp7_allele_not = True
+        elif entry.patient_rna and entry.allelic.value is ALLELIC.FALSE.value:
+            if entry.quantification <= 0.05:
+                bp7 = True
             else:
-                bp7_no += 1
+                bp7_no = True
     if not rna_data:
         performed = False
         result = False
@@ -149,22 +193,24 @@ def assess_splicing_data_bp7(
     elif no_quantification == len(rna_data):
         result = False
         comment = "No quantification available for the RNA assay. Please check results of RNA assay manually."
-    elif (bp7_allele_not > 0 or bp7_no > 0) and (bp7 > 0 or bp7_allele > 0):
+    elif (bp7_allele_not or bp7_no) and (bp7 or bp7_allele):
         result = False
-        comment = "Multiple RNA Assays were performed with contradictory results. Please check results manually."
-    elif bp7_allele_not > 0:
+        comment = "Multiple RNA Assays were performed with contradictory or inconclusive results. Please check results of RNA assay manually."
+    elif bp7_allele_not:
         result = False
-        comment = "The performed RNA assays with allele specific quantification or minigene assay, do not show the variant to have no effect on splicing."
-    elif bp7_no > 0:
+        comment = "The performed RNA assays with allele specific quantification or minigene assay do not show the variant to have no effect on splicing."
+    elif bp7_no:
         result = False
-        comment = "The performed RNA assays without allele specific quantification, do not show the variant to have no effect splicing."
+        comment = "The performed RNA assays without allele specific quantification do not show the variant to have no effect splicing."
     elif bp7_allele:
         result = True
-        comment = "The performed RNA assay with allel specific quantification or minigene assay, shows the variant not to have an effect on splicing."
+        comment = "The performed RNA assay with allel specific quantification or minigene assay shows the variant not to have an effect on splicing."
     elif bp7:
         result = True
-        comment = "The performed RNA assay without allel specific quantification, shows the variant not to have an effect on splicing."
+        comment = "The performed RNA assay without allel specific quantification shows the variant not to have an effect on splicing."
     else:
         result = False
-        comment = "RNA assay is inconclusive. Please check results manually."
+        comment = (
+            "RNA assay is inconclusive. Please check results of RNA assay manually."
+        )
     return performed, result, comment
