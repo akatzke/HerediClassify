ca529e7fcfd894c1d7d5987567c8bfa9550b037e
akatzke <al.katzke@gmail.com>
2024-03-13 08:42:57 +0100

    Fixed bug regarding strand identification

--- variant_classification/check_coldspot_hotspot.py
+++ variant_classification/check_coldspot_hotspot.py
@@ -5,6 +5,7 @@ import pathlib
 from collections.abc import Callable
 
 import pyensembl
+import logging
 
 from pybedtools import BedTool
 
@@ -12,6 +13,8 @@ from ensembl import ensembl
 from information import Classification_Info, Info
 from variant import TranscriptInfo, VariantInfo
 from utils import create_bed_line
+
+logger = logging.getLogger("GenOtoScope_Classify.check_coldspot_hotspot")
 
 
 def check_variant_intersection_with_bed(
@@ -23,11 +26,11 @@ def check_variant_intersection_with_bed(
     Check hotspot annotation file for location of variant
     """
     # The reference transcript is only needed for the strand, therefore the specific transcript does not matter
-    ref_transcript = ensembl.transcript_by_id(transcript[0].transcript_id)
+    strand = get_variant_strand(transcript, variant)
     var_start = variant.genomic_start
     var_end = variant.genomic_end
     is_in_hotspot = check_intersection_with_bed_no_strand(
-        variant, var_start, var_end, ref_transcript, variant_hotspot_annotation_path
+        variant, var_start, var_end, strand, variant_hotspot_annotation_path
     )
     return is_in_hotspot
 
@@ -68,14 +71,14 @@ def check_intersection_with_bed_no_strand(
     variant: VariantInfo,
     gen_start: int,
     gen_end: int,
-    ref_transcript: pyensembl.transcript.Transcript,
+    strand: str,
     path_bed: pathlib.Path,
 ) -> bool:
     """
     Check if variant overlaps region in given bed file without checking for strand
     """
     variant_interval = BedTool(
-        create_bed_line(variant, gen_start, gen_end, ref_transcript.strand),
+        create_bed_line(variant, gen_start, gen_end, strand),
         from_string=True,
     )[0]
     bed = BedTool(path_bed).sort()
@@ -83,3 +86,39 @@ def check_intersection_with_bed_no_strand(
     if len(annotation_hits) > 0:
         return True
     return False
+
+
+def get_variant_strand(transcripts: list[TranscriptInfo], variant: VariantInfo) -> str:
+    """
+    Try to get strand for gene
+    """
+    try:
+        # Try getting strand from transcript
+        ref_transcript = ensembl.transcript_by_id(transcripts[0].transcript_id)
+        strand = ref_transcript.strand
+        return strand
+    except Exception:
+        # In case getting strand from transcript fails, try getting strand from gene
+        try:
+            genes = ensembl.genes_by_name(variant.gene_name)
+            for gene in genes:
+                # Check if variant location matches genen location
+                if (
+                    gene.contig in gene.chr
+                    and (
+                        variant.genomic_start >= gene.start
+                        and gene.end >= variant.genomic_start
+                    )
+                    and (
+                        gene.end >= variant.genomic_end
+                        and variant.genomic_end >= gene.start
+                    )
+                ):
+                    return gene.strand
+            raise ValueError(
+                f"None of the genes identified for {variant.gene_name} could be matched to the variant location {variant.chr}:{variant.genomic_start}-{variant.genomic_end}."
+            )
+        except Exception:
+            raise ValueError(
+                "The reconstruction of the variant strand failed. Please check."
+            )
