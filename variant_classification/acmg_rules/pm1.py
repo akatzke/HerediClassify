93c2023792a2df9a59211642dee1f3eb34abd385
Athena <al.katzke@gmail.com>
2024-07-12 19:02:03 +0200

    Added splice prediction check PM1_TP53

--- variant_classification/acmg_rules/pm1.py
+++ variant_classification/acmg_rules/pm1.py
@@ -11,6 +11,7 @@ from acmg_rules.utils import (
 )
 from information import Classification_Info, Info
 from variant import PopulationDatabases
+from acmg_rules.computation_evidence_utils import Threshold, assess_thresholds
 
 
 class Pm1(abstract_rule):
@@ -92,6 +93,8 @@ class Pm1_tp53(abstract_rule):
                 class_info.VARIANT_HOTSPOT_ANNOTATION,
                 class_info.VARIANT_CANCERHOTSPOTS,
                 class_info.THRESHOLD_CANCERHOTSPOTS_AC,
+                class_info.VARIANT_PREDICTION,
+                class_info.THRESHOLD_SPLICING_PREDICTION_BENIGN,
             ),
         )
 
@@ -101,7 +104,11 @@ class Pm1_tp53(abstract_rule):
         variant_in_hotspot: bool,
         cancerhotspots: PopulationDatabases,
         threshold_cancerhotspots_ac: float,
+        prediction_dict: dict[str, float],
+        threshold: Threshold,
     ) -> RuleResult:
+        prediction_value = prediction_dict.get(threshold.name, None)
+        num_thresholds_met = assess_thresholds(threshold, prediction_value)
         if variant_in_hotspot:
             comment = f"Variant in defined mutational hotspot."
             result = True
@@ -114,6 +121,19 @@ class Pm1_tp53(abstract_rule):
         else:
             comment = f"Variant not in mutational hotspot and occurence of variant in Cancer Hotspots ({cancerhotspots.count}) does not meet threshold ({threshold_cancerhotspots_ac})."
             result = False
+        if num_thresholds_met is None:
+            comment = (
+                "ATTENTION: No splicing prediction available for this variant. "
+                + comment
+            )
+        elif num_thresholds_met == 0:
+            comment = "Variant is not predicted to not affect splicing, therefore PM1 does not apply."
+            result = False
+        else:
+            comment = (
+                comment
+                + " {threshold.name} predicts no effect on splicing for the variant (threshold: {threshold.thresholds[num_thresholds_met -1]}, value: {prediction_value})."
+            )
         return RuleResult(
             "PM1",
             rule_type.GENERAL,
