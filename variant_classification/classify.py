8f0a5284feca7e968f2ebdaa95a11b495f368bc6
akatzke <al.katzke@gmail.com>
2025-01-07 09:26:34 +0100

    Added final classification to classify

--- variant_classification/classify.py
+++ variant_classification/classify.py
@@ -47,10 +47,11 @@ def classify(config_path: pathlib.Path, variant_str: str) -> tuple[dict, str]:
     )
     rule_results = apply_rules(annotations_needed_by_rules_filtered)
     rule_dict = create_rules_dict(rule_results)
-    rule_results_checked = check_incompatible_rules(
+    rule_dict_checked = check_incompatible_rules(
         rule_dict, final_config["name"], final_config["rules"]
     )
-    out_result = create_output(rule_results_checked)
+    rule_final_class = get_final_classifications(rule_dict_checked, final_config)
+    out_result = create_output(rule_final_class)
     ensembl.clear_cache()
     return final_config, out_result
 
